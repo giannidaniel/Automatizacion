d8abb5afcfe10e2f7ca8ef86118a1e4f4d914039
juancella <51832964+juancella@users.noreply.github.com>
2019-06-19 09:22:14 -0300

    Agregada función para el encoding utf-16

--- carga.py
+++ carga.py
@@ -1,6 +1,6 @@
 class carga():
     #Esta funcion pasandole el parametro "ruta" con la ruta del archivo, lo lee y lo guarda en la variable lista
-    def cargador(self,ruta):
+    def cargador_utf8(self,ruta):
         self.archivo = open(ruta, 'r', encoding = 'utf-8')
         Lista = self.archivo.read()
         self.archivo.close()
@@ -8,3 +8,9 @@ class carga():
 
        #def limpiador():
         
+    def cargador_utf16(self,nombre):
+        self.nombre=nombre
+        with open(self.nombre,"r",encoding='utf-16') as self.archivo:
+        Lista = self.archivo.read()
+        self.archivo.close()
+        return Lista
