019aa6787c830dc3f5dc6d0b66d4aaa90aad6869
juancella <51832964+juancella@users.noreply.github.com>
2019-06-25 11:29:24 -0300

    Update C08.ps1

--- Scripts/C08.ps1
+++ Scripts/C08.ps1
@@ -1,4 +1,4 @@
 $server = $env:COMPUTERNAME
-GPRESULT /S $server /scope computer /H salida.html
-Get-Content salida.html | set-content -Encoding utf8 C08_$server.html
-Remove-Item salida.html
+GPRESULT /S $server /scope computer /H salida.txt
+Get-Content salida.txt | set-content -Encoding utf8 C08_$server.txt
+Remove-Item salida.txt
