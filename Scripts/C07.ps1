449d4fb357fe9c39ee1c0d66901110908768a9f5
juancella <51832964+juancella@users.noreply.github.com>
2019-06-25 09:02:53 -0300

    Update C07.ps1
    Se cambió el formato del nombre del archivo de salida a "Control"_"Nombre del servidor".txt

--- Scripts/C07.ps1
+++ Scripts/C07.ps1
@@ -1,3 +1,3 @@
 #Obtiene el Path en donde se encuentra el parametro Autodisconnect y lo exporta en un archivo de C07.txt
-
-(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters -name Autodisconnect).Autodisconnect | Out-File C07.txt -Encoding 'utf8'
+$computer_name = $env:COMPUTERNAME
+(Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters -name Autodisconnect).Autodisconnect | Out-File C07_$computer_name.txt -Encoding 'utf8'
