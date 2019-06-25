fd7c45459f03cebbf50195d1f686577aadc11acd
juancella <51832964+juancella@users.noreply.github.com>
2019-06-25 08:55:47 -0300

    Update C06.ps1
    Se cambió el formato del nombre del archivo de salida a "Control"_"Nombre del servidor".txt

--- Scripts/C06.ps1
+++ Scripts/C06.ps1
@@ -1,1 +1,2 @@
-gwmi Win32_LogicalDisk | select-object -property name,FileSystem,drivetype| Where-Object {$_.drivetype -eq 3 }| Out-File C06.txt -Encoding 'utf8'
+$computer_name = $env:COMPUTERNAME
+gwmi Win32_LogicalDisk | select-object -property name,FileSystem,drivetype| Where-Object {$_.drivetype -eq 3 }| Out-File C06_$computer_name.txt -Encoding 'utf8'
