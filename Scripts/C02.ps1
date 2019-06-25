80c9da5c152d178dfacf1eb08ee50f4adc4f9fbc
juancella <51832964+juancella@users.noreply.github.com>
2019-06-25 09:05:14 -0300

    Update C02.ps1

--- Scripts/C02.ps1
+++ Scripts/C02.ps1
@@ -1,20 +1,12 @@
 $computer_name = $env:COMPUTERNAME
-
 $usuarios = @()
-
-
 $usuarios_locales = get-wmiobject -class win32_useraccount | select-object name
-
-
 $usuarios_finales = @()
 
 foreach($usuario in $usuarios_locales)
 {	
 	$usuarios_finales += $usuario.name
-}
-
-	
+}	
 
 $usuarios += $usuarios_finales | ForEach-Object { cmd.exe /c net user "$_" } 
-
 $usuarios  | Out-File C02_$computer_name.txt -Encoding 'utf8'
