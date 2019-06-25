1b91d351e856ec52698a7717225ae022f72b7f1d
juancella <51832964+juancella@users.noreply.github.com>
2019-06-25 09:03:21 -0300

    Update C12.ps1
    Se cambió el formato del nombre del archivo de salida a "Control"_"Nombre del servidor".txt

--- Scripts/C12.ps1
+++ Scripts/C12.ps1
@@ -1,3 +1,4 @@
+$computer_name = $env:COMPUTERNAME
 secedit.exe /export /areas SECURITYPOLICY /cfg filename.txt
-Get-Content filename.txt | Set-Content -Encoding utf8 C12.txt
+Get-Content filename.txt | Set-Content -Encoding utf8 C12_$computer_name.txt
 Remove-Item filename.txt
